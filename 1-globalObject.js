db6e1c1d5b25dccc133187e7427e38ecf55f12b9
Aswath Siva <aswathsiva0420@gmail.com>
2026-06-16 20:01:10 +0530

    1-Global object

---
+++ 1-globalObject.js
@@ -0,0 +1,11 @@
+const hello = (name) => {
+    console.log(`Hello, ${name} !`);
+}
+
+hello("Aswath");
+
+console.log(global); // Prints what the browser can do using node js
+
+
+console.log(__dirname); // Prints folder name
+console.log(__filename); // Prints with file name
